b88ee01cb23fb5f330d45596817d340973a2e608
Sylvain Laperche <sylvain.laperche@gmx.fr>
2014-01-13 18:40:59 +0800

    Update copyright

--- test/ts_CalCal.rb
+++ test/ts_CalCal.rb
@@ -1,5 +1,9 @@
 #encoding: utf-8
 
+# Copyright (c) 2013-2014, Sylvain LAPERCHE
+# All rights reserved.
+# License: BSD 3-Clause (http://opensource.org/licenses/BSD-3-Clause)
+
 require 'simplecov'
 SimpleCov.start
 
